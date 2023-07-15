2215b177f72962d37cf46765546d2209dcf15f9e
Muhammed Saeed <musaeed@tony-2.coli.uni-saarland.de>
2023-07-15 03:11:12 +0200

    dev

--- dev/scripts/devData.py
+++ dev/scripts/devData.py
@@ -3,29 +3,35 @@ import pandas as pd
 
 
 #tsv file path
-tsv = "/local/musaeed/train.tsv"
+tsv = "/local/musaeed/CLaT/train.tsv"
 
 
 # Initialize an empty list
-lines_list = []
 
-# Read the text file
-with open('/local/musaeed/pcm_nsc-ud-dev.conllu', 'r') as file:
-    # Iterate over each line in the file
-    for line in file:
-        # Check if the line starts with '# text_en ='
-        if line.startswith('# text_en ='):
-            # Remove the '# text_en =' prefix and append the line to the list
-            lines_list.append(line[len('# text_en ='):].strip())
+def returnTreenbank(file):
+    # Read the text file
+    lines_list = []
+    with open(file, 'r') as file:
+        # Iterate over each line in the file
+        for line in file:
+            # Check if the line starts with '# text_en ='
+            if line.startswith('# text_en ='):
+                # Remove the '# text_en =' prefix and append the line to the list
+                lines_list.append(line[len('# text_en ='):].strip())
 
-# Print the resulting list
-print(lines_list)
+    # Print the resulting list
+    # print(lines_list)
+    return lines_list
 df = pd.read_csv(tsv, sep='\t')
 
-check_list = lines_list
-filtered_df = df[~(df['input_text'].isin(check_list) | df['target_text'].isin(check_list))]
+check_list1 = returnTreenbank('/local/musaeed/pcm_nsc-ud-dev.conllu')
+print(len(check_list1))
+
+check_list1.extend(returnTreenbank('/local/musaeed/pcm_nsc-ud-test.conllu'))
+print(len(check_list1))
+filtered_df = df[~(df['input_text'].isin(check_list1) | df['target_text'].isin(check_list1))]
 print(filtered_df.head())
 
 print(f"the len difference ois {len(df) - len(filtered_df)}")
 
-filtered_df.to_csv("/local/musaeed/CLaT/dev/data/trainDatawithoutDev.tsv",sep='\t')
+filtered_df.to_csv("/local/musaeed/CLaT/dev/data/trainDatawithoutDevNorTest.tsv",sep='\t')
